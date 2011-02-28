25c14e0d578b9aefc11c24f379d58fe8d7853a54
rickerbh <hamish@glimmerdesign.com>
2011-02-28 19:04:03 +0000

    Remove private method and replace with version that uses GCD, not NSThread (from comment on http://blog.logichigh.com/2010/09/12/cocoa-blocks/) Tidied code to look the way I like it.

--- NSURLConnection-Blocks.h
+++ NSURLConnection-Blocks.h
@@ -9,6 +9,10 @@
 #import <Foundation/Foundation.h>
 
 @interface NSURLConnection (Blocks)
+
 #pragma mark Class API Extensions
-+ (void)asyncRequest:(NSURLRequest *)request success:(void(^)(NSData *,NSURLResponse *))successBlock_ failure:(void(^)(NSData *,NSError *))failureBlock_;
++ (void)asyncRequest:(NSURLRequest *)request 
+						 success:(void(^)(NSData *,NSURLResponse *))successBlock_ 
+						 failure:(void(^)(NSData *,NSError *))failureBlock_;
+
 @end
--- NSURLConnection-Blocks.m
+++ NSURLConnection-Blocks.m
@@ -3,35 +3,25 @@
 @implementation NSURLConnection (Blocks)
 
 #pragma mark API
-+ (void)asyncRequest:(NSURLRequest *)request success:(void(^)(NSData *,NSURLResponse *))successBlock_ failure:(void(^)(NSData *,NSError *))failureBlock_
-{
-	[NSThread detachNewThreadSelector:@selector(backgroundSync:) toTarget:[NSURLConnection class]
-						   withObject:[NSDictionary dictionaryWithObjectsAndKeys:
-									   request,@"request",
-									   successBlock_,@"success",
-									   failureBlock_,@"failure",
-									   nil]];
-}
-
-#pragma mark Private
-+ (void)backgroundSync:(NSDictionary *)dictionary
-{
-	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	void(^success)(NSData *,NSURLResponse *) = [dictionary objectForKey:@"success"];
-	void(^failure)(NSData *,NSError *) = [dictionary objectForKey:@"failure"];
-	NSURLRequest *request = [dictionary objectForKey:@"request"];
-	NSURLResponse *response = nil;
-	NSError *error = nil;
-	NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-	if(error)
-	{
-		failure(data,error);
-	}
-	else
-	{
-		success(data,response);
-	}
-	[pool release];
++ (void)asyncRequest:(NSURLRequest *)request 
+						 success:(void(^)(NSData *,NSURLResponse *))successBlock_ 
+						 failure:(void(^)(NSData *,NSError *))failureBlock_ {
+	dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+		NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+		NSURLResponse *response = nil;
+		NSError *error = nil;
+		NSData *data = [NSURLConnection sendSynchronousRequest:request 
+																				 returningResponse:&response 
+																										 error:&error];
+		
+		if (error) {
+			failureBlock_(data,error);
+		} else {
+			successBlock_(data,response);
+		}
+		
+		[pool release];
+	});
 }
 
 @end
